f6c9360098640f7a098cef3e5ba1f091e66b3560
Denny LI Guang <hikarulea+github@gmail.com>
2017-11-10 11:36:59 +0800

    Fix bug for bat files
    Move all " to Function

--- Script_2.CopyFilesIntoCorrespondingFolders.bat
+++ Script_2.CopyFilesIntoCorrespondingFolders.bat
@@ -6,21 +6,28 @@ set target_dir=%current_dir%\FolderTree
 
 :: Project
 :: ...\FolderTree\!__Markdown\!--XXX Project.md
-set file_name="!--XXX Project.md"
+set file_name=!--XXX Project.md
 set target_detail_dir=%target_dir%\!__Markdown
 call:myCopy
 
 :: Project
 :: ...\FolderTree\!__Markdown\!--XXX Project_Todo-List(Archived).md
-set file_name="!--XXX Project_Todo-List(Archived).md"
+set file_name=!--XXX Project_Todo-List(Archived).md
 set target_detail_dir=%target_dir%\!__Markdown
 call:myCopy
 
 :: AMS
-::...\FolderTree\00.AMS Support\README.md
+::...\FolderTree\00.AMS Support\README_AMS Support.md
 set file_name_src=AMS_README.md
-set file_name_tgt=README.md
+set file_name_tgt=README_AMS Support.md
 set target_detail_dir=%target_dir%\00.AMS Support
+call:myCopy2
+
+:: [Report or Project Name]_Template
+::...\FolderTree\01.[Report or Project Name]_Template\README.md
+set file_name_src=[Report or Project Name]_README.md
+set file_name_tgt=README_[Report or Project Name].md
+set target_detail_dir=%target_dir%\01.[Report or Project Name]_Template
 call:myCopy2
 
 :: Dev
@@ -30,31 +37,31 @@ set file_name_tgt=README.md
 set target_detail_dir=%target_dir%\70.DEV(OverAll)
 call:myCopy2
 ::...\FolderTree\70.DEV(OverAll)\!--XXX BW & BO --!.md
-set file_name="!--XXX BW & BO --!.md"
+set file_name=!--XXX BW & BO --!.md
 set target_detail_dir=%target_dir%\70.DEV(OverAll)
 call:myCopy
 
 ::...\FolderTree\!_SourceCode\ABAP_Class\[Class Name]_YYYYMMDD.md
-set file_name="[Class Name]_YYYYMMDD.md"
+set file_name=[Class Name]_YYYYMMDD.md
 set target_detail_dir=%target_dir%\!_SourceCode\ABAP_Class
 call:myCopy
 
 ::...\FolderTree\!_SourceCode\ABAP_Program\[Program Name]_YYYYMMDD.md
-set file_name="[Program Name]_YYYYMMDD.md"
+set file_name=[Program Name]_YYYYMMDD.md
 set target_detail_dir=%target_dir%\!_SourceCode\ABAP_Program
 call:myCopy
 
 :: Transport
 :: ...\FolderTree\71.MTP(TR Status)\!--XXX TR Status - BO.md
-set file_name="!--XXX TR Status - BO.md"
+set file_name=!--XXX TR Status - BO.md
 set target_detail_dir=%target_dir%\71.MTP(TR Status)
 call:myCopy
 :: ...\FolderTree\71.MTP(TR Status)\!--XXX TR Status - BW.md
-set file_name="!--XXX TR Status - BW.md"
+set file_name=!--XXX TR Status - BW.md
 set target_detail_dir=%target_dir%\71.MTP(TR Status)
 call:myCopy
 :: ...\FolderTree\71.MTP(TR Status)\!--XXX TR Status - DO NOT Transport.md
-set file_name="!--XXX TR Status - DO NOT Transport.md"
+set file_name=!--XXX TR Status - DO NOT Transport.md
 set target_detail_dir=%target_dir%\71.MTP(TR Status)
 call:myCopy
 :: ...\FolderTree\71.MTP(TR Status)\XXX_TR_Status_YYYYMM(Archives).md
@@ -66,17 +73,17 @@ pause
 goto:eof
 
 :myCopy
-set source="%source_dir%"\%file_name%
+set source="%source_dir%\%file_name%"
 echo %source%
-set target="%target_detail_dir%"\%file_name%
+set target="%target_detail_dir%\%file_name%"
 echo %target%
 copy /y %source% %target%
 goto:eof
 
 :myCopy2
-set source="%source_dir%"\%file_name_src%
+set source="%source_dir%\%file_name_src%"
 echo %source%
-set target="%target_detail_dir%"\%file_name_tgt%
+set target="%target_detail_dir%\%file_name_tgt%"
 echo %target%
 copy /y %source% %target%
 goto:eof
